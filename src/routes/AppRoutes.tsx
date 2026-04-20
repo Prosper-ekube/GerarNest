316117c2b758be647acb00a5b279e76242066af9
Prosper-ekube <prosperekube@gmail.com>
2026-04-20 06:50:13 +0100

    feat(routing): add route for Product Details page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import Product from '../pages/product/Product'
+import ProductDetails from '../pages/product/sections/ProductDetails'
 import Contact from "../pages/contact/Contact"
 import AdminProducts from '../pages/admin/AdminProducts'
 
@@ -10,6 +11,7 @@ const AppRoutes = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/product' element={<Product />} />
+                <Route path='/products/:id' element={<ProductDetails />} />
                 <Route path='/contact' element={<Contact />} />
                 <Route path='/admin/products' element={<AdminProducts />} />
             </Routes>
