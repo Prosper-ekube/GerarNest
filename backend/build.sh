07cc5a68c6bc221034ed86fe309e2ebd6a820eb5
Prosper-ekube <prosperekube@gmail.com>
2026-08-27 15:44:50 +0100

    fix(backend): correct product fixture loading order

--- backend/build.sh
+++ backend/build.sh
@@ -4,5 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py migrate
+python manage.py shell -c "from products.models import Product; Product.objects.all().delete()"
 python manage.py loaddata products
 python manage.py collectstatic --noinput
